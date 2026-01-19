8de4aeb230922064bd77a318c8924be43e75bdd5
jeevikas3117 <jeevikas31@gmail.com>
2026-01-19 22:13:44 +0530

    Added

---
+++ Easy/_15_getIntersectionNode.c
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    if (headA == NULL || headB == NULL)
+        return NULL;
+
+    struct ListNode *pA = headA;
+    struct ListNode *pB = headB;
+
+    while (pA != pB) {
+        if (pA == NULL){
+            pA = headB;
+        }
+        else{
+            pA = pA->next;
+        }
+        if (pB == NULL){
+            pB = headA;
+        }
+        else{
+            pB = pB->next;
+        }
+    }
+
+    return pA;   // either intersection node or NULL
+}
