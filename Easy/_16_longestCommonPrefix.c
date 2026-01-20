23eff07190bbdc5b1696544d7c9257b3c21c1932
jeevikas3117 <jeevikas31@gmail.com>
2026-01-20 23:18:15 +0530

    Added

---
+++ Easy/_16_longestCommonPrefix.c
@@ -0,0 +1,18 @@
+char* longestCommonPrefix(char** strs, int strsSize) {
+    if (strsSize == 0){
+        return "";
+    }
+    char* prefix = strs[0];
+    for (int i = 1; i < strsSize; i++) {
+        int j = 0;
+        while (prefix[j] != '\0' &&
+               strs[i][j] != '\0' &&
+               prefix[j] == strs[i][j]) {
+            j++;
+        }
+        prefix[j] = '\0';   // shorten prefix
+        if (prefix[0] == '\0')
+            return "";
+    }
+    return prefix;
+}
