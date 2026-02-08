41b4040a66c297864b651f7d712e5fe76e2b6626
jeevikas3117 <jeevikas31@gmail.com>
2026-02-08 18:51:10 +0530

    Added

---
+++ Easy/_35_checkPerfectNumber.c
@@ -0,0 +1,12 @@
+bool checkPerfectNumber(int num) {
+    long int sum=0;
+    for(int i=1;i<=num/2;i++){
+        if(num%i==0){
+            sum+=i;
+        }
+    }
+    if(sum==num){
+        return true;
+    }
+    return false;
+}
