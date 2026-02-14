420992791de0d9aa1131ecbb40dcae94cee442bc
jeevikas3117 <jeevikas31@gmail.com>
2026-02-14 18:57:09 +0530

    Added

---
+++ Easy/_41_isPerfectSquare.c
@@ -0,0 +1,11 @@
+bool isPerfectSquare(int num) {
+    for(long long int i=1;i<=num;i++){
+        if(i*i==num){
+            return true;
+        }
+        if(i*i>num){
+            break;
+        }
+    }
+    return false;
+}
