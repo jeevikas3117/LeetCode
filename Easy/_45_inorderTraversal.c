9da6b24d2d364adef3fcd28ff6edaa2dafafb594
jeevikas3117 <jeevikas31@gmail.com>
2026-02-18 10:06:57 +0530

    Added

---
+++ Easy/_45_inorderTraversal.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+ void helper(struct TreeNode* root, int* arr, int* index) {
+    if (root == NULL)
+        return;
+    helper(root->left, arr, index);
+    arr[*index] = root->val;
+    (*index)++;
+    helper(root->right, arr, index);
+    
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* arr = (int*)malloc(sizeof(int) * 100);
+    int index = 0;
+    helper(root, arr, &index);
+    *returnSize = index;
+    return arr;
+}
