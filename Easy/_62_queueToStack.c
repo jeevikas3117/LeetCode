ebeb015ededf2d45fd5e9b638886b714c643f11e
jeevikas3117 <jeevikas31@gmail.com>
2026-03-12 09:37:53 +0530

    Added

---
+++ Easy/_62_queueToStack.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX 100
+
+typedef struct {
+    int stack1[MAX];
+    int stack2[MAX];
+    int top1;
+    int top2;
+} MyQueue;
+
+MyQueue* myQueueCreate() {
+    MyQueue* obj = (MyQueue*)malloc(sizeof(MyQueue));
+    obj->top1 = -1;
+    obj->top2 = -1;
+    return obj;
+}
+
+void myQueuePush(MyQueue* obj, int x) {
+    obj->stack1[++obj->top1] = x;
+}
+
+void transfer(MyQueue* obj) {
+    while(obj->top1 != -1) {
+        obj->stack2[++obj->top2] = obj->stack1[obj->top1--];
+    }
+}
+
+int myQueuePop(MyQueue* obj) {
+    if(obj->top2 == -1) {
+        transfer(obj);
+    }
+    return obj->stack2[obj->top2--];
+}
+
+int myQueuePeek(MyQueue* obj) {
+    if(obj->top2 == -1) {
+        transfer(obj);
+    }
+    return obj->stack2[obj->top2];
+}
+
+bool myQueueEmpty(MyQueue* obj) {
+    return (obj->top1 == -1 && obj->top2 == -1);
+}
+
+void myQueueFree(MyQueue* obj) {
+    free(obj);
+}
